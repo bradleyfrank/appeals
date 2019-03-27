7f4fd0bdef3b6231d617698d2072777ae64d959f
Brad <bradley.frank@gmail.com>
2019-03-26 22:10:10 -0400

    Fixes mimetype detection.

--- download.py
+++ download.py
@@ -1,53 +1,66 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 __author__ = 'Bradley Frank'
 
 import argparse
 import logging
+import magic
+import mimetypes
 import os
 import shutil
 import subprocess
 import sys
+import tempfile
 import urllib.request
 
 base_url = 'https://www.sec.state.ma.us/AppealsWeb/Download.aspx?DownloadPath='
-download_path = '/home/bfrank/Downloads/Public_Records'
+download_path = '/Users/bfrank/Downloads/Public_Records'
 
-def get_record(fileID):
+def get_record(file_ID, temp_directory):
 
-    download_url = base_url + fileID
-    #print('Downloading file ' + fileID + '.')
+    download_url = base_url + file_ID
 
     try:
-        document = urllib.request.urlopen(download_url)
+        response = urllib.request.urlopen(download_url)
     except HTTPError as e:
-        print('Could not download file ID ' + fileID + '.')
+        print('Could not download file ID ' + file_ID + '.')
         return False
     except URLError as e:
-        print('Could not download file ID ' + fileID + '.')
+        print('Could not download file ID ' + file_ID + '.')
         return False
 
-    filename = os.path.join(download_path, fileID + '.pdf')
+    tmpfile = os.path.join(temp_directory, file_ID)
 
-    # Create repo directory if it doesn't exist
-    if not os.path.isdir(download_path):
-        os.makedirs(download_path, exist_ok=True)
-
-    # Skip if file already exists
-    if os.path.isfile(filename):
-        print('File already downloaded.')
-        return False
-
-    # Save the document to disk
     try:
-        with open(filename, 'wb') as f:
-            shutil.copyfileobj(document, f)
+        with open(tmpfile, 'wb') as f:
+            shutil.copyfileobj(response, f)
     except IOError as e:
         print(e)
         return False
 
+    mime = magic.from_file(tmpfile, mime=True)
+
+    if mime is None:
+        return False
+    elif mime == 'application/msword':
+        ext = '.doc'
+    elif mime == 'application/pdf':
+        ext = '.pdf'
+    else:
+        ext = mimetypes.guess_extension(mime, True)
+
+    filename = os.path.join(download_path, file_ID + ext)
+
+    # Create download directory if it doesn't exist
+    if not os.path.isdir(download_path):
+        os.makedirs(download_path, exist_ok=True)
+
+    # Move the temp file into place
+    os.rename(tmpfile, filename)
+
     return True
 
-for d in range(10950, 11000):
-    fileID = str(d)
-    get_record(fileID)
+temp_directory = tempfile.mkdtemp()
+for d in range(1, 5):
+    file_ID = str(d)
+    get_record(file_ID.zfill(5), temp_directory)
